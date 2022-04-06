30ebdd2eb3b6b90dbe0a8977dff6f619d9a487e8
David W Stuckey <311993@dc=wcs,dc=local>
2022-04-06 08:12:32 -0400

    room parse work

--- src/game/Room.java
+++ src/game/Room.java
@@ -1,18 +1,22 @@
 package game;
 
+import java.io.File;
+
+import util.RoomNotFoundException;
+
 public class Room{
 	
 	private int x, y, width, height;
 	private String name;
-	private String data;
+	private int[][] data;
 	
-	public Room(int x, int y, int width, int height, String name, int levelNum) {
+	public Room(int x, int y, int width, int height, String name, int levelNum) throws RoomNotFoundException{
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.name = name;
-		this.data = 
+		this.data = util.DatManager.datToArray(new File("src\\data\\level" + levelNum + ".dat")); 
 	}
 	
 	public Room(int x, int y, int width, int height, int levelNum) {
