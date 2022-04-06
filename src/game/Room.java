3de7fd89c0e20aaf5587bb4eb9828de99e201a49
David W Stuckey <311993@dc=wcs,dc=local>
2022-04-06 07:53:43 -0400

    start room parsing method

--- src/game/Room.java
+++ src/game/Room.java
@@ -4,18 +4,22 @@ public class Room{
 	
 	private int x, y, width, height;
 	private String name;
+	private String data;
 	
-	public Room(int x, int y, int width, int height, String name) {
+	public Room(int x, int y, int width, int height, String name, int levelNum) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.name = name;
+		this.data = 
 	}
 	
-	public Room(int x, int y, int width, int height) {
-		this(x, y, width, height, "");
+	public Room(int x, int y, int width, int height, int levelNum) {
+		this(x, y, width, height, "", levelNum);
 	}
 	
-	
+	public String toString(){
+		return width + " x " + height + " room at " + x + ", " + y;
+	}
 }
